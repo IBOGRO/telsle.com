9e3a1275ca11b43238701e69be736a109b68940d
IBOGRO <92876959+IBOGRO@users.noreply.github.com>
2024-07-29 20:50:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,23 +3,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const resultDiv = document.getElementById('result');
 
     button.addEventListener('click', () => {
-        fetch("https://cashblox.gg/_next/data/W0mAusPnDdTf3CPPSZbDp/profile.json", {
+        const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+        const targetUrl = 'https://cashblox.gg/_next/data/W0mAusPnDdTf3CPPSZbDp/profile.json';
+
+        fetch(proxyUrl + targetUrl, {
             headers: {
                 "sec-ch-ua": "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Google Chrome\";v=\"126\"",
                 "sec-ch-ua-mobile": "?0",
                 "sec-ch-ua-platform": "\"Windows\"",
                 "x-nextjs-data": "1"
             },
-            referrer: "https://cashblox.gg/surveys",
-            referrerPolicy: "strict-origin-when-cross-origin",
-            body: null,
             method: "GET",
             mode: "cors",
             credentials: "omit"
         })
         .then(response => response.json())
         .then(data => {
-            // Process and display the data
             resultDiv.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
         })
         .catch(error => {
