f905f96fd281dde98a65ef0e33b9b2d1266e4d70
IBOGRO <92876959+IBOGRO@users.noreply.github.com>
2024-07-29 21:10:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,40 +4,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
     button.addEventListener('click', () => {
         const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-        const targetUrl = 'https://cashblox.gg/api/chat/post';
+        const targetUrl = 'https://cashblox.gg/_next/data/W0mAusPnDdTf3CPPSZbDp/profile.json';
 
         fetch(proxyUrl + targetUrl, {
-            method: 'POST',
             headers: {
-                'Content-Type': 'text/plain',
-                'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
-                'sec-ch-ua-mobile': '?0',
-                'sec-ch-ua-platform': '"Windows"'
+                "sec-ch-ua": "\"Not/A)Brand\";v=\"8\", \"Chromium\";v=\"126\", \"Google Chrome\";v=\"126\"",
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": "\"Windows\"",
+                "x-nextjs-data": "1"
             },
-            body: 'hi'
+            method: "GET",
+            mode: "cors",
+            credentials: "omit"
         })
-        .then(response => response.text()) // Adjusted to text for simplicity
+        .then(response => response.json())
         .then(data => {
-            // Send the response to the Discord webhook
-            return fetch('https://discord.com/api/webhooks/1267513254717161544/YlRJQW2tbDJFR7rXPeKGpdcHlBREAyqyJ97QjRzZ7QRQKnfrVOL0j3ccyJro2FQFR4l4', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    content: `Received response: ${data}`
-                })
-            });
-        })
-        .then(discordResponse => {
-            if (!discordResponse.ok) {
-                throw new Error('Failed to send response to Discord webhook');
-            }
-            resultDiv.innerHTML = 'Response successfully sent to Discord!';
+            resultDiv.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
         })
         .catch(error => {
-            console.error('Error:', error);
-            resultDiv.innerHTML = 'Error sending data.';
+            console.error('Error fetching data:', error);
+            resultDiv.innerHTML = 'Error fetching data.';
         });
     });
 });
